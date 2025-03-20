cf0ec278183df8f7e59d138401b7b4ea0afcace4
Youri Pullens <>
2025-03-20 22:33:13 +0100

    added new image and prevented email snooping

---
+++ content/js/script.js
@@ -0,0 +1,9 @@
+// prevent email snooping
+document.addEventListener('DOMContentLoaded', function() {
+    const emailElements = document.querySelectorAll('.protected-email');
+    emailElements.forEach(function(element) {
+        const user = element.getAttribute('data-user');
+        const domain = element.getAttribute('data-domain');
+        element.innerHTML = `<a href="mailto:${user}@${domain}">${user}@${domain}</a>`;
+    });
+});
